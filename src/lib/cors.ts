9d800eac5a54a43c7f9cae0ae81932d6a7e37d32
karukimori-wq <karukimori@gmail.com>
2026-08-16 16:36:45 +0900

    Allow PATCH in CORS

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+  "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, X-Trace-Id, X-Correlation-Id, X-Source-App",
   "Access-Control-Max-Age": "86400"
 };
